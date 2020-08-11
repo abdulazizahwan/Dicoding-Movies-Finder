a1ab42a96fd544ed0cb9ec0de6b035dd21faffd7
abdulazizahwan <abdulazizahwan97@gmail.com>
2020-08-11 13:26:34 +0700

    update

--- src/index.js
+++ src/index.js
@@ -3,10 +3,4 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/main.css";
 import main from "./scripts/view/main";
 
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3000
-app.get('/', (req, res) => res.send('<h2> Hello World! </h2>'));
-app.listen(PORT, () => console.log(`app listening on port ${PORT}`))
-
 main();
