ec6926e60cfdb17e88c584fc56089d7da81c9b99
abdulazizahwan <abdulazizahwan97@gmail.com>
2020-08-11 13:16:34 +0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import main from "./scripts/view/main";
 
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 3000
 app.get('/', (req, res) => res.send('<h2> Hello World! </h2>'));
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`))
 
