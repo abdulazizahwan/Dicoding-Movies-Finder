2325e5fd3ef55750d75b743eba3a60e475b70a16
abdulazizahwan <abdulazizahwan97@gmail.com>
2020-08-11 13:05:30 +0700

    add express js and update index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/main.css";
 import main from "./scripts/view/main";
 
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 4000
+app.get('/', (req, res) => res.send('<h2> Hello World! </h2>'));
+app.listen(PORT, () => console.log(`app listening on port ${PORT}`))
+
 main();
